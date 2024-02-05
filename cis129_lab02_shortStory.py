ab2b2ac336317d57185a09a68165ff9dcecf69b3
arobles54 <158803807+arobles54@users.noreply.github.com>
2024-02-05 06:00:26 -0700

    First Python Lab in CIS129 - Short Story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+# Author: Ana Robles
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print(Once upon a time, ina small town named there was a blue cat named "Berry".)
+print(Berry had a special gift, everytime that he purred, blueberries would mysteriously appear around him.)
+print(This brought the townsfolk great joy.)
+print(Leading the townsfolk to name the town "Berrytown", after Berry the blue cat.)
